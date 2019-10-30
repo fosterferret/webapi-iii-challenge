b4dc99995d9bf90ca9743978e31a1215c68d933e
Francis Bulus <notchera@gmail.com>
2019-10-30 15:12:49 +0100

    created endpoint for getting specific user

--- users/userRouter.js
+++ users/userRouter.js
@@ -28,6 +28,14 @@ router.post("/:id/posts", validateUserId, (req, res) => {
 });
 
 router.get("/", (req, res) => {
+  Users.get(req.query)
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+      res.status(500).json({ message: "Error retrieving the users" });
+    });
+});
+
+router.get("/:id", (req, res) => {
   Users.getById(req.params.id)
     .then(user => {
       if (user) {
@@ -37,14 +45,11 @@ router.get("/", (req, res) => {
       }
     })
     .catch(err => {
-      console.log(err);
       res
         .status(500)
         .json({ message: "encountered an error getting the user" });
     });
 });
-
-router.get("/:id", (req, res) => {});
 
 router.get("/:id/posts", (req, res) => {});
 
