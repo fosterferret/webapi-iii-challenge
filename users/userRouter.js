9d25fb1b02e2e4e0735163ab9efc37fbe08cc187
Francis Bulus <notchera@gmail.com>
2019-10-30 15:10:35 +0100

    created endpoint for retreiving users

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,7 +3,7 @@ const Users = require("./userDb");
 const Posts = require("../posts/postDb");
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", validateUser, (req, res) => {
   Users.insert(req.body)
     .then(user => res.status(201).json(user))
     .catch(err => {
@@ -13,7 +13,7 @@ router.post("/", (req, res) => {
     });
 });
 
-router.post("/:id/posts", (req, res) => {
+router.post("/:id/posts", validateUserId, (req, res) => {
   const { id } = req.params;
   const postDetails = { ...req.body, user_id: id };
   Posts.insert(postDetails)
@@ -21,16 +21,28 @@ router.post("/:id/posts", (req, res) => {
       res.status(210).json(post);
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          error:
-            "we encountered an error while creating this post for the user"
-        });
+      res.status(500).json({
+        error: "we encountered an error while creating this post for the user"
+      });
     });
 });
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  Users.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "user not found" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "encountered an error getting the user" });
+    });
+});
 
 router.get("/:id", (req, res) => {});
 
@@ -63,7 +75,7 @@ function validateUser(req, res, next) {
   if (!Object.keys(req.body).length) {
     res.status(400).json({ message: "missing user data!" });
   } else if (!req.body.name) {
-    res.status(400).json({ message: 'Missing required "name" field!' });
+    res.status(400).json({ message: 'missing required "name" field!' });
   } else {
     next();
   }
