0e05eb222d686bbdd44988ea25a60618afe96af6
Francis Bulus <notchera@gmail.com>
2019-10-30 14:39:24 +0100

    defined server and port in index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+// code away!
+
+const server = require("./server");
+const port = 4000;
+server.listen(port, () =>
+  console.log(`\n*** server is running on port:${port} ***\n`)
+);
